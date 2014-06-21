7aaf7e681fd138c530f9b86ca7799d1c9d82df25
Guy Morita <guy@hipmunk.com>
2014-06-21 12:02:37 -0700

    Initial Commit

---
+++ smitter/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  smitter
+//
+//  Created by Guy Morita on 6/21/14.
+//  Copyright (c) 2014 geemoo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
